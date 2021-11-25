e902b13be9f1d2645f82f67fd6fb54e062ef7a3b
Seth Mwebi <sethmwebi27@gmail.com>
2021-11-25 23:39:13 +0300

    bug fix

--- components/Header.js
+++ components/Header.js
@@ -4,56 +4,57 @@ import { useRouter } from "next/router";
 import { useRef } from "react";
 import { XIcon, MicrophoneIcon, SearchIcon } from "@heroicons/react/solid";
 import Avatar from "./Avatar";
-import HeaderOptions from "./HeaderOptions"
+import HeaderOptions from "./HeaderOptions";
 
 const Header = () => {
-	const router = useRouter();
-	const searchInputRef = useRef();
+  const router = useRouter();
+  const searchInputRef = useRef();
 
-	const search = (e) => {
-		e.preventDefault();
-		const term = searchInputRef.current.value;
-		if (!term) return;
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+    if (!term) return;
 
-		router.push(`/search?term=${term}`);
-	};
-	return (
-		<header className="sticky top-0 bg-white">
-			<div className="flex w-full p-6 items-center">
-				<Image
-					src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
-					height={40}
-					width={120}
-					onClick={() => router.push("/")}
-					className="cursor-pointer"
-				/>
-				<form className="flex flex-1 px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
-					<input
-						className="flex-1 w-full focus:outline-none"
-						ref={searchInputRef}
-					/>
-					<XIcon
-						className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
-						onClick={() => {
-							searchInputRef.current.value = "";
-						}}
-					/>
-					<MicrophoneIcon className="h-6 mr-3 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
-					<SearchIcon className="h-6 text-blue-500 hidden sm:inline-flex" />
-					<button hidden type="submit" onClick={search}>
-						Search
-					</button>
-				</form>
-				<Avatar
-					className="ml-auto"
-					url="https://lh3.googleusercontent.com/ogw/ADea4I71sKWPhwl-t7bmHYf_qMqH32XlJcw6bb89awdvqQ=s32-c-mo"
-				/>
-			</div>
+    router.push(`/search?term=${term}`);
+  };
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center">
+        <Image
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
+          height={40}
+          width={120}
+          onClick={() => router.push("/")}
+          className="cursor-pointer"
+        />
+        <form className="flex flex-1 px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+          <input
+            className="flex-1 w-full focus:outline-none"
+            ref={searchInputRef}
+            defaultValue={router.query.term}
+          />
+          <XIcon
+            className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+            onClick={() => {
+              searchInputRef.current.value = "";
+            }}
+          />
+          <MicrophoneIcon className="h-6 mr-3 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
+          <SearchIcon className="h-6 text-blue-500 hidden sm:inline-flex" />
+          <button hidden type="submit" onClick={search}>
+            Search
+          </button>
+        </form>
+        <Avatar
+          className="ml-auto"
+          url="https://lh3.googleusercontent.com/ogw/ADea4I71sKWPhwl-t7bmHYf_qMqH32XlJcw6bb89awdvqQ=s32-c-mo"
+        />
+      </div>
 
-			{/*HeaderOptions*/}
-			<HeaderOptions />
-		</header>
-	);
+      {/*HeaderOptions*/}
+      <HeaderOptions />
+    </header>
+  );
 };
 
 export default Header;
--- keys.js
+++ keys.js
@@ -3,4 +3,3 @@ export const API_KEY = "AIzaSyDH0rhbOrUpB4aVYRMwJQ3bFFl6Ywz92q4";
 
 // Context Key
 export const CONTEXT_KEY = "f490c8d9edb3197ff";
-
